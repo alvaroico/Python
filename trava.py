d91062a1e98e5d96ec0c6dc9f4c788f2e11a1b39
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2020-09-23 21:10:09 -0300

    Trava

---
+++ trava.py
@@ -0,0 +1,11 @@
+for a in range(0, 9):
+    for b in range(0, 9):
+        for c in range(0, 9):
+            for d in range(0, 9):
+                for e in range(0, 9):
+                    for f in range(0, 9):
+                        for g in range(0, 9):
+                            for h in range(0, 9):
+                                for i in range(0, 9):
+                                    for j in range(0, 9):
+                                        print(a,b,c,d,e,f,g,h,i,j)
